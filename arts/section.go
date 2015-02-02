7c615d7fe883801aa72db9d5e30b4ca66efbeee1
Ben Campbell <ben@scumways.com>
2015-02-02 16:56:57 +1300

    add evil special-case section hacks for itv, sky

--- arts/section.go
+++ arts/section.go
@@ -13,13 +13,26 @@ import (
 
 // eg <meta property="article:section" content="Politics" />
 var sectionSels = struct {
-	meta      cascadia.Selector
-	script    cascadia.Selector
-	ftSectPat *regexp.Regexp
+	meta cascadia.Selector
 }{
 	cascadia.MustCompile(`head meta[property="article:section"], head meta[name="Section"]`),
+}
+
+// evil special-case hacks for various specific sites
+var evilSectionHacks = struct {
+	script       cascadia.Selector
+	ftJSPat      *regexp.Regexp
+	skyNewsJSPat *regexp.Regexp
+	itvSel       cascadia.Selector
+}{
 	cascadia.MustCompile(`script`),
+	// siteMapTerm = 'Sections.Technology';
 	regexp.MustCompile(`siteMapTerm = '(?:.*)[.](.*)';`),
+	// window.skynews.config.analytics.section = 'politics/Leaders Await Grilling On Issues Facing Young';
+	regexp.MustCompile(`analytics[.]section = '(.*)/.*';`),
+
+	// <li class="tag-list__tag tag-list__tag--category"><a href="/news/health/">Health</a></li>
+	cascadia.MustCompile(`.tag-list__tag--category a`),
 }
 
 // returns "" if no section found
@@ -42,23 +55,40 @@ func grabSection(root *html.Node, u *url.URL) string {
 		i++
 	}
 	section := strings.Join(out, ", ")
-	if section == "" && strings.ToLower(u.Host) == "www.ft.com" {
-		return ftSection(root)
+	if section == "" {
+		// no section found - try out assorted evil special-case per-site hacks
+		switch strings.ToLower(u.Host) {
+		case "www.ft.com":
+			return sectionFromJavascript(root, evilSectionHacks.ftJSPat)
+		case "news.sky.com":
+			return sectionFromJavascript(root, evilSectionHacks.skyNewsJSPat)
+		case "www.itv.com":
+			return sectionFromElement(root, evilSectionHacks.itvSel)
+		}
 	}
 
 	return section
 }
 
-// evil little special-case to get a section out of an FT article...
-// look for javascript, eg:
-// siteMapTerm = 'Sections.Technology';
-func ftSection(root *html.Node) string {
-	for _, el := range sectionSels.script.MatchAll(root) {
+// Evil little special-case to get an article section out javascript.
+// Sites often embed metadata in javascript which isn't marked up any other way.
+// Usually, this is extra data is used for advertising. Sigh.
+func sectionFromJavascript(root *html.Node, pat *regexp.Regexp) string {
+	for _, el := range evilSectionHacks.script.MatchAll(root) {
 		txt := getTextContent(el)
-		m := sectionSels.ftSectPat.FindStringSubmatch(txt)
+		m := pat.FindStringSubmatch(txt)
 		if m != nil {
 			return strings.TrimSpace(strings.ToLower(m[1]))
 		}
 	}
 	return ""
+}
+
+// grab text of matching selector, return as one line
+func sectionFromElement(root *html.Node, sel cascadia.Selector) string {
+	el := sel.MatchFirst(root)
+	if el == nil {
+		return ""
+	}
+	return compressSpace(getTextContent(el))
 }
